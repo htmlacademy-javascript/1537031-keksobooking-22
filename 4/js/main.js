968b15c40715fdbd8cce175b185dfd25692d0f84
Keks <keks@htmlacademy.ru>
2021-02-12 10:06:16 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,10 +1,1 @@
-import { getOffer } from './data.js';
-const ADDS_COUNT = 10;
-
-const offers = [];
-
-for (let i = 0; i < ADDS_COUNT; i++) {
-  offers.push(getOffer());
-}
-
-console.log(offers);
+import './data.js';
