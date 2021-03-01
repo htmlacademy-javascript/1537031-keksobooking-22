ae72468791c4fd73ccc4efd1fc8324c7e2447e17
Keks <keks@htmlacademy.ru>
2021-03-01 22:39:17 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/api.js
+++ 10/js/api.js
@@ -1,44 +1,47 @@
-const GET_URL = 'https://22.javascript.pages.academy/keksobooking/data';
-const POST_URL = 'https://22.javascript.pages.academy/keksobooking';
+/* 1.Я тут не очень поняла идею. Получилось, что fetch обернули в функцию, которая делает то же самое, что fetch
+* 2. Сейчас без перезагрузки страницы форму успешно можно отправить
+* только один раз. После закрытия попапа, если снова запролнить форму и нажать Опубликовать,
+* ничего не происходит. Не могу найти причину*/
 
-const getData = (onSuccess, onFail) => {
-  fetch(GET_URL)
-    .then((response) => {
-      if(response.ok) {
+const getData = (url) => {
+  return fetch(url);
+}
+
+const processData = (url, onSuccess, onFail) => {
+  getData(url)
+    .then ((response => {
+      if (response.ok) {
         return response.json();
-      } else {
-        onFail(`Не удалось загрузить данные об объектах: ${response.text}`);
       }
-    })
-    .then((advertisements) => {
-      onSuccess(advertisements);
-    })
-    .catch(() => {
-      onFail('Не удалось загрузить данные об объектах.');
-    });
-};
+      onFail(`Не удалось загрузить данные об объектах: ${response.text}`);
+    }))
+    .then(onSuccess)
+    .catch(onFail);
+}
 
-const sendData = (onSuccess, onFail, body) => {
-  fetch(
-    POST_URL,
+/*Здесь тоже не поняла. По сути дублируем функционал fetch*/
+const sendData = (url, body) => {
+  return fetch(
+    url,
     {
       method: 'POST',
       body,
     },
   )
+}
+
+const postData = (url, onSuccess, onFail, body) => {
+  sendData(url, body)
     .then((response) => {
       if (response.ok) {
-        onSuccess();
-      } else {
-        onFail();
+        return onSuccess();
       }
-    })
-    .catch(() => {
       onFail();
-    });
+    })
+    .catch(onFail);
 };
 
-export { getData, sendData };
+export { processData, postData };
 
 
 
