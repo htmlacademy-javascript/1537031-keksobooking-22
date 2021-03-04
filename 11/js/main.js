801e0982e02e93e7a7b36dfefa256049a9ff8ba6
Keks <keks@htmlacademy.ru>
2021-03-04 20:10:18 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,52 @@
+import './data.js';
+import './cards.js';
+import './form.js';
+import './map.js';
+import './api.js';
+import {
+  resetMainPinMarker,
+  setUpMap,
+  STARTING_LATITUDE,
+  STARTING_LONGITUDE,
+} from './map.js';
+import { getData } from './api.js';
+import { ADDS_COUNT, MIN_ADDS } from './data.js';
+import { showAlert } from './util.js';
+import {
+  adForm,
+  adFormResetButton,
+  advertisementFormSubmit,
+  deactivateMapForm,
+  fillAddress,
+  onResetAdForm
+} from './form.js';
+import { showErrorModal, showSuccessModal } from './success-modal.js';
+import { deactivateFilter, filterAdvertisements } from './filter.js';
+
+const GET_URL = 'https://22.javascript.pages.academy/keksobooking/data';
+
+const setDefaults = () => {
+  adForm.reset();
+  resetMainPinMarker();
+  onResetAdForm();
+  fillAddress(STARTING_LATITUDE, STARTING_LONGITUDE);
+}
+
+deactivateMapForm();
+deactivateFilter();
+
+getData(GET_URL, (advertisements) => {
+  setUpMap(advertisements.slice(MIN_ADDS, ADDS_COUNT));
+  filterAdvertisements(advertisements);
+}, showAlert('Не удалось загрузить данные об объектах'))
+
+advertisementFormSubmit(() => {
+  showSuccessModal();
+  setDefaults();
+}, showErrorModal);
+
+adFormResetButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  setDefaults();
+});
+
